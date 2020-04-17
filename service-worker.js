f3fb4cf60d6c503f35d6b4ee7a61e5d4c0fd90f6
liuzhe <liu748951@163.com>
2020-04-17 23:57:04 +0800

    1.UPDATE——修改data文件数据结构,添加component键；将catalog修正为section,将section修正为chapter；section数据简化,删掉href只保留title,并将对象数组改为字符串数组 2.图片压缩 3.NEW——目录组件 4.UPDATE——修改文档目录结构;删除Chapters文件夹,并将内容移动至文档根目录下;00-09.js重命名为0-9.js；删除index.js 5.UPDATE——将内容区组件改为数据map生成方式 6.UPDATE——随data文件的修改将Menu、PageFooter、Sider组件,utils/keymap.js做出适应性调整

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4d2b4994ef4c93f35a15b4156268a153.js"
+  "./precache-manifest.9cb3aab9ef5d79d49a23d36be4c72ff5.js"
 );
 
 self.addEventListener('message', (event) => {
